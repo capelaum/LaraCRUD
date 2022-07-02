b174f0f6c2370911bf41a2dc0717d6e612221db7
Luis V. Capelletto <thecapellett@gmail.com>
2022-07-02 19:22:31 -0300

    🎨 format views

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -1,4 +1,5 @@
-@extends('layouts.default') @section('title', 'Admin Produtos')
+@extends('layouts.default')
+@section('title', 'Admin Produtos')
 @section('content')
 
 <section class="text-gray-600">
@@ -9,42 +10,29 @@
                     Produtos
                 </h1>
                 <a
-                    class="flex ml-auto text-white bg-indigo-500 border-0 py-1.5 px-3 text-sm focus:outline-none hover:bg-indigo-600 rounded"
-                    >Adicionar</a
-                >
+                    class="flex ml-auto text-white bg-indigo-500 border-0 py-1.5 px-3 text-sm focus:outline-none hover:bg-indigo-600 rounded">Adicionar</a>
             </div>
             <table class="table-auto w-full text-left whitespace-no-wrap">
                 <thead>
                     <tr>
-                        <th
-                            class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"
-                        >
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
                             #
                         </th>
-                        <th
-                            class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"
-                            style="width: 150px"
-                        >
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"
+                            style="width: 150px">
                             Imagem
                         </th>
-                        <th
-                            class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"
-                        >
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
                             Nome
                         </th>
-                        <th
-                            class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"
-                        >
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
                             Valor
                         </th>
-                        <th
-                            class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"
-                        >
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
                             Estoque
                         </th>
                         <th
-                            class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 text-right"
-                        >
+                            class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 text-right">
                             Ações
                         </th>
                     </tr>
@@ -53,51 +41,29 @@
                     <tr>
                         <td class="px-4 py-3">1</td>
                         <td class="px-4 py-3">
-                            <img
-                                alt="ecommerce"
-                                class="object-cover object-center w-full h-full block"
-                                src="https://dummyimage.com/800x450"
-                            />
+                            <img alt="ecommerce" class="object-cover object-center w-full h-full block"
+                                src="https://dummyimage.com/800x450" />
                         </td>
                         <td class="px-4 py-3">Produto 1</td>
                         <td class="px-4 py-3">R$10</td>
                         <td class="px-4 py-3">10</td>
-                        <td
-                            class="px-4 py-3 text-sm text-right space-x-3 text-gray-900"
-                        >
-                            <a
-                                class="mt-3 text-indigo-500 inline-flex items-center"
-                                >Editar</a
-                            >
-                            <a
-                                class="mt-3 text-indigo-500 inline-flex items-center"
-                                >Deletar</a
-                            >
+                        <td class="px-4 py-3 text-sm text-right space-x-3 text-gray-900">
+                            <a class="mt-3 text-indigo-500 inline-flex items-center">Editar</a>
+                            <a class="mt-3 text-indigo-500 inline-flex items-center">Deletar</a>
                         </td>
                     </tr>
                     <tr class="bg-gray-50">
                         <td class="px-4 py-3">2</td>
                         <td class="px-4 py-3">
-                            <img
-                                alt="ecommerce"
-                                class="object-cover object-center w-full h-full block"
-                                src="https://dummyimage.com/800x450"
-                            />
+                            <img alt="ecommerce" class="object-cover object-center w-full h-full block"
+                                src="https://dummyimage.com/800x450" />
                         </td>
                         <td class="px-4 py-3">Produto 2</td>
                         <td class="px-4 py-3">R$10</td>
                         <td class="px-4 py-3">10</td>
-                        <td
-                            class="px-4 py-3 text-sm text-right space-x-3 text-gray-900"
-                        >
-                            <a
-                                class="mt-3 text-indigo-500 inline-flex items-center"
-                                >Editar</a
-                            >
-                            <a
-                                class="mt-3 text-indigo-500 inline-flex items-center"
-                                >Deletar</a
-                            >
+                        <td class="px-4 py-3 text-sm text-right space-x-3 text-gray-900">
+                            <a class="mt-3 text-indigo-500 inline-flex items-center">Editar</a>
+                            <a class="mt-3 text-indigo-500 inline-flex items-center">Deletar</a>
                         </td>
                     </tr>
                 </tbody>
